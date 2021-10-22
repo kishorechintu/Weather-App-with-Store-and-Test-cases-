989b23d9b8ecf69a233886ae1120dceb59c61d2c
Kishore Kumar Bolla <Kishore29.bolla@gmail.com>
2021-10-22 19:29:47 +0530

    Added change detection stragy in weather card comonent

--- src/app/components/weather-card/weather-card.component.ts
+++ src/app/components/weather-card/weather-card.component.ts
@@ -1,21 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
 import { WeatherAppSate } from 'src/app/store/weater-app-state';
 import { getWeather } from 'src/app/store/weather-selector';
-import { WeatherDataModel } from 'src/app/weather.model';
 
 @Component({
   selector: 'app-weather-card',
   templateUrl: './weather-card.component.html',
   styleUrls: ['./weather-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class WeatherCardComponent implements OnInit {
-  public weatherData$!: Observable<WeatherDataModel[]>;
-
+export class WeatherCardComponent {
   constructor(private store: Store<{ weahter: WeatherAppSate }>) {}
 
-  ngOnInit() {
-    this.weatherData$ = this.store.select(getWeather);
-  }
+  weatherData$ = this.store.select(getWeather);
 }
